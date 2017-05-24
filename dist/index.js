8634a4ffb6a97f32cbf27a6be8f71838ce398ad0
Nick Rigby <nick.rigby@gmail.com>
2017-05-23 20:48:47 -0400

    More abstracted class functions. Stop iframe videos when closing.

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,59 @@
 "use strict";
 exports.__esModule = true;
 var SilcOffcanvas = (function () {
+    /**
+     * Constructor
+     * @param element
+     */
     function SilcOffcanvas(element) {
         var _this = this;
-        // Save shortcut to element
-        this.element = element;
-        this.element.addEventListener('click', function (event) {
-            _this.toggle(event);
-        });
+        // Shortcut to trigger
+        this.trigger = element;
+        // Get target element
+        this.target = this.getTarget(this.trigger);
+        // Event listener for trigger
+        if (this.target !== undefined) {
+            this.trigger.addEventListener('click', function (e) {
+                _this.toggle(e);
+            });
+        }
     }
+    /**
+     * Get target element for trigger
+     * @param trigger
+     */
+    SilcOffcanvas.prototype.getTarget = function (trigger) {
+        if (!trigger.hasAttribute('href')) {
+            console.log('silc offcanvas: Trigger is missing href attribute');
+            return undefined;
+        }
+        var targetSelector = trigger.getAttribute('href');
+        var targetEl = document.querySelector(targetSelector);
+        if (targetEl === undefined) {
+            console.log('silc offcanvas: Target element ' + targetSelector + ' does not exist');
+        }
+        return targetEl;
+    };
+    /**
+     * Stop target iframe videos from playing
+     * @param target
+     */
+    SilcOffcanvas.prototype.stopIframeVideo = function (target) {
+        [].forEach.call(target.querySelectorAll('iframe'), function (el) {
+            var src = el.getAttribute('src');
+            el.setAttribute('src', src);
+        });
+    };
+    /**
+     * Toggle visibility of the target
+     * @param event
+     */
     SilcOffcanvas.prototype.toggle = function (event) {
         event.preventDefault();
-        var target = event.target.getAttribute('href');
-        var targetEl = document.querySelector(target);
-        targetEl.classList.toggle('silc-offcanvas--visible');
+        if (this.target.classList.contains('silc-offcanvas--visible')) {
+            this.stopIframeVideo(this.target);
+        }
+        this.target.classList.toggle('silc-offcanvas--visible');
     };
     return SilcOffcanvas;
 }());
